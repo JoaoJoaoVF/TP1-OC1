1086c0415136c5094cd40606020fff44cb8a0432
João Vitor Ferreira <jvferreira.20@gmail.com>
2022-10-12 22:06:47 -0300

    cpf

--- joao_vitor_2021039654/prob2.s
+++ joao_vitor_2021039654/prob2.s
@@ -1,5 +1,5 @@
 .data
-vetor: .word 0 2 1 3 6 2 2 5 6 4 2
+vetor: .word 7 9 1 8 1 7 4 0 6 2 0
 ##### START MODIFIQUE AQUI START #####
 #addi x13, x0, 11 # marca o registrador 13 como 11
 ##### END MODIFIQUE AQUI END #####
@@ -13,32 +13,42 @@ main:
 ##### START MODIFIQUE AQUI START #####
 
 primeiraiteracao:
-	lw x6, 0(x12)
-	mul x6, x6, x15
-    add x17, x17, x6
-    #sw x6, 0(x12)
-	addi x15, x15, -1
-	#addi x14, x14, 1
-	addi x12, x12, 4
-    bne x15, x14, primeiraiteracao
-    jalr x0, 0(x1)
+	lw x6, 0(x12) # carrega o elemento do cpf
+	mul x6, x6, x15 # realiza a multiplicacao dele por a k, que vai de 10 a 2, decrescendo 1 unidade a cada passo, comecando na primeira posicao do CPF e indo ate a 9
+    add x17, x17, x6 # soma o resultado da multiplicação acima com as anteriores e guarda em x17
+	addi x15, x15, -1 # diminui 1 de k
+	addi x12, x12, 4 # anda para o proximo algarismo do CPF
+    bne x15, x14, primeiraiteracao # enquanto nao chegar na posicao 9 realiza novamente a funcao
+    jalr x0, 0(x1) # retorna para a funcao verifica CPF
 
 
 segundainteracao:
-	lw x5, 0(x12)
-	mul x5, x5, x13
-    add x18, x18, x5
-    #sw x6, 0(x12)
-	addi x13, x13, -1
-	#addi x14, x14, 1
-	addi x12, x12, 4
-    bne x13, x14, segundainteracao
-    jalr x0, 0(x1)
-    
-verificadigito:
+	lw x5, 0(x12)  # carrega o elemento do cpf
+	mul x5, x5, x13 # realiza a multiplicacao dele por a j, que vai de 11 a 2, decrescendo 1 unidade a cada passo, comecando na primeira posicao do CPF e indo ate a 10
+    add x18, x18, x5 # soma o resultado da multiplicação acima com as anteriores e guarda em x18 
+	addi x13, x13, -1 # diminui 1 de j
+	addi x12, x12, 4 # anda para o proximo algarismo do CPF
+    bne x13, x14, segundainteracao  # enquanto nao chegar na posicao 10 realiza novamente a funcao
+    jalr x0, 0(x1) # retorna para a funcao verifica CPF
+
+falso: # da para reutilizar!!!!
+	addi x10, x10, 0 # coloca a resposta como falsa
+    jalr x0, 0(x1) # retorna para a funcao verifica CPF 
+
+verificadigito: # da para reutilizar!!!! eu acho
 	mul x17, x17, x15 # realiza a multiplicacao por 10 dos primeiros 9 elementos
+    rem x17, x17, x13 # realiza o mod de 11
     mul x18, x18, x15 # realiza a multiplicacao por 10 dos primeiros 10 elementos
-    jalr x0, 0(x1)	
+    rem x18, x18, x13 # realiza o mod de 11
+    addi x12, x12, -40# volta a posicao do x12 para a primeira
+    lw x7, 36(x12) # carrega o digito na posicao 10
+    lw x8, 40(x12) # carrega o digito na posica 11
+    rem x18, x18, x15 # realiza o mod de 10 para normalizar valores maiores que 10
+    rem x17, x17, x15 # realiza o mod de 10 para normalizar valores maiores que 10
+    bne x17, x7, falso # se o digito na posicao 10 for diferente do calculado, retorna falso
+    bne x18, x8, falso # se o digito na posicao 11 for diferente do calculado, retorna falso
+   	addi x10, x10, 1 # se nao for, retorna verdadeiro
+    jalr x0, 0(x1) # retorna para a funcao verifica CPF
     
     
 verificacpf: 
@@ -46,24 +56,21 @@ verificacpf:
     add x18, x0, x0 # guarda a soma de todos os valores do vetor na segunda rodada
 	addi x14, x0, 1 # o limite minimo que o vetor deve realizar as somas
 	addi x13, x0, 11 # aloca a quantidade de elementos que o vetor tem
-    #add x13, x13, x12 # adiciona o inicio do vetor a x13
     addi x15, x0, 10 # coloca 10 o valor 10 em x14
-    #slli x15, x15, 2 
-    # sw x15, 0(x12) # guarda o valor de x15 em na primeira pos de x12
-    sw x16, 0(x1)
-    jal x1, primeiraiteracao
+    sw x16, 0(x1) # salva o valor de x16 na pilha, para retorno
+    jal x1, primeiraiteracao # chama a primeira parte do algoritmo
     addi x12, x12, -36 # volta a posicao do x12 para a primeira
-    jal x1, segundainteracao
+    jal x1, segundainteracao # chama a segunda parte do algoritmo
     addi x13, x0, 11 # coloca 11 em x13 novamente para realizar o MOD
     addi x15, x0, 10 # coloca 10 em x13 novamente para realizar a multiplicação
-    jal x1, verificadigito
-    lw x1, 0(x16)
-    beq x0, x0, retorna
+    jal x1, verificadigito #chama a funcao que verifica se os digitos sao iguais aos calculados
+    lw x1, 0(x16) # carrega o valor para retorno 
+    beq x0, x0, retorna # chama a funcao para sair da analise do CPF
 
 verificacnpj: 
 
 
-retorna:
+retorna: # da para reutilizar!!!!
     jalr x0, 0(x1) # realiza o retorno da funcao
 
 verificadastro: 
